1bffb635b6db2a3c5e0ca5ca841c5c073bc0c12a
vinh.nguyen <vinh.nguyen@linecorp.com>
2024-11-27 12:11:49 +0700

    Jetpack Compose, Chapter 5: Combining Composables

--- JetpackComposePlayGround/kodeco-jetnotes/src/main/java/com/yourcompany/android/jetnotes/MainActivity.kt
+++ JetpackComposePlayGround/kodeco-jetnotes/src/main/java/com/yourcompany/android/jetnotes/MainActivity.kt
@@ -5,8 +5,17 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.Scaffold
+import androidx.compose.material.ScaffoldState
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.rememberCoroutineScope
+import com.yourcompany.android.jetnotes.routing.Screen
+import com.yourcompany.android.jetnotes.theme.JetNotesTheme
+import com.yourcompany.android.jetnotes.ui.components.AppDrawer
+import com.yourcompany.android.jetnotes.ui.components.Note
 import com.yourcompany.android.jetnotes.viewmodel.MainViewModel
 import com.yourcompany.android.jetnotes.viewmodel.MainViewModelFactory
+import kotlinx.coroutines.launch
 
 /**
  * Main activity for the app.
@@ -28,7 +37,25 @@ class MainActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
 
     setContent {
-
+      JetNotesTheme {
+        val coroutineScope = rememberCoroutineScope()
+        val scaffoldState: ScaffoldState = rememberScaffoldState()
+        Scaffold(
+          scaffoldState = scaffoldState,
+          drawerContent = {
+            AppDrawer(
+              currentScreen = Screen.Notes,
+              onScreenSelected = { screen ->
+                coroutineScope.launch {
+                  scaffoldState.drawerState.close()
+                }
+              }
+            )
+          }
+        ) {
+          Note()
+        }
+      }
     }
   }
 }
