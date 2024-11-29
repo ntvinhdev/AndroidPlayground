8d8150ca2dc086c2a197652e1ff7e14b6a6cc79d
Vinh Nguyen <ntvinh.dev@gmail.com>
2024-11-29 11:17:16 +0700

    Revert "[Jetpack Compose by Tutorials] Chapter 4, Add project JetNotes"

--- JetpackComposePlayGround/kodeco-jetnotes/src/main/java/com/yourcompany/android/jetnotes/theme/Color.kt
+++
@@ -1,7 +0,0 @@
-package com.yourcompany.android.jetnotes.theme
-
-import androidx.compose.ui.graphics.Color
-
-val green = Color(0xFF006837)
-val greenDark = Color(0xFF004012)
-val red = Color(0xFFC75F00)
--- JetpackComposePlayGround/kodeco-jetnotes/src/main/java/com/yourcompany/android/jetnotes/util/Extensions.kt
+++
@@ -1,7 +0,0 @@
-package com.yourcompany.android.jetnotes.util
-
-import androidx.compose.ui.graphics.Color
-
-fun Color.Companion.fromHex(hex: String): Color {
-  return Color(android.graphics.Color.parseColor(hex))
-}
