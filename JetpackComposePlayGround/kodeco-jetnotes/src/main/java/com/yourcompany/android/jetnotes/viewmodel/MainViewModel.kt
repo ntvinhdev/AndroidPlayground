19f0c378f19d5d50613534cffe7c4a3fd070ec72
vinh.nguyen <vinh.nguyen@linecorp.com>
2024-12-04 19:19:16 +0700

    Jetpack Compose, Chapter 7: Managing State in Compose

--- JetpackComposePlayGround/kodeco-jetnotes/src/main/java/com/yourcompany/android/jetnotes/viewmodel/MainViewModel.kt
+++ JetpackComposePlayGround/kodeco-jetnotes/src/main/java/com/yourcompany/android/jetnotes/viewmodel/MainViewModel.kt
@@ -1,7 +1,13 @@
 package com.yourcompany.android.jetnotes.viewmodel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import com.yourcompany.android.jetnotes.data.repository.Repository
+import com.yourcompany.android.jetnotes.domain.model.NoteModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 /**
  * View model used for storing the global app state.
@@ -10,4 +16,21 @@ import com.yourcompany.android.jetnotes.data.repository.Repository
  */
 class MainViewModel(private val repository: Repository) : ViewModel() {
 
+  val notesNoteInTrash: LiveData<List<NoteModel>> by lazy {
+    repository.getAllNotesNotInTrash().asLiveData()
+  }
+
+  fun onCreateNewNoteClick() {
+
+  }
+
+  fun onNoteClick() {
+
+  }
+
+  fun onNoteCheckedChange(note: NoteModel) {
+    viewModelScope.launch(Dispatchers.Default) {
+      repository.insertNote(note)
+    }
+  }
 }
