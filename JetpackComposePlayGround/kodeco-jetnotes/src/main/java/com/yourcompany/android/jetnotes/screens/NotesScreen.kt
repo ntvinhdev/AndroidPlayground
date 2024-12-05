0f7a51cd089dbd54e19e06b53c676d1d51f48e4e
vinh.nguyen <vinh.nguyen@linecorp.com>
2024-12-05 12:14:51 +0700

    Jetpack Compose, Chapter 7: Managing State in Compose

--- JetpackComposePlayGround/kodeco-jetnotes/src/main/java/com/yourcompany/android/jetnotes/screens/NotesScreen.kt
+++ JetpackComposePlayGround/kodeco-jetnotes/src/main/java/com/yourcompany/android/jetnotes/screens/NotesScreen.kt
@@ -1,9 +1,38 @@
 package com.yourcompany.android.jetnotes.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.List
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import com.yourcompany.android.jetnotes.domain.model.NoteModel
+import com.yourcompany.android.jetnotes.ui.components.Note
+import com.yourcompany.android.jetnotes.ui.components.TopAppBar
 import com.yourcompany.android.jetnotes.viewmodel.MainViewModel
 
 @Composable
 fun NotesScreen(viewModel: MainViewModel) {
+  val notes: List<NoteModel> by viewModel
+    .notesNoteInTrash
+    .observeAsState(listOf())
 
+  Column {
+    TopAppBar(
+      title = "JetNotes",
+      icon = Icons.Filled.List,
+      onIconClick = {}
+    )
+    LazyColumn {
+      items(count = notes.size) { noteIndex ->
+        val note = notes[noteIndex]
+        Note(
+          note = note,
+          onNoteClick = { viewModel.onNoteClick() },
+          onNoteCheckedChange = { viewModel.onNoteCheckedChange(it) }
+        )
+      }
+    }
+  }
 }
