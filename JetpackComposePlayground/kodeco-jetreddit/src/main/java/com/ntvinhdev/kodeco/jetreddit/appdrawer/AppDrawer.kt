ae93e93d9b69940c93f0059572a7503b43795a28
vinh.nguyen <vinh.nguyen@linecorp.com>
2024-12-13 17:39:44 +0700

    Jetpack Compose by Tutorials, Chapter 9: Using ConstraintSets in Composables

--- JetpackComposePlayground/kodeco-jetreddit/src/main/java/com/ntvinhdev/kodeco/jetreddit/appdrawer/AppDrawer.kt
+++ JetpackComposePlayground/kodeco-jetreddit/src/main/java/com/ntvinhdev/kodeco/jetreddit/appdrawer/AppDrawer.kt
@@ -2,22 +2,36 @@ package com.ntvinhdev.kodeco.jetreddit.appdrawer
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Divider
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.ntvinhdev.kodeco.jetreddit.routing.Screen
 import com.ntvinhdev.kodeco.jetreddit.theme.JetRedditThemeSettings
 import com.ntvinhdev.kodeco.jetreddit.R
@@ -36,9 +50,7 @@ fun AppDrawer(
       .background(color = MaterialTheme.colors.surface)
   ) {
     AppDrawerHeader()
-
     AppDrawerBody(onScreenSelected)
-
     AppDrawerFooter(modifier)
   }
 }
@@ -48,12 +60,77 @@ fun AppDrawer(
  */
 @Composable
 private fun AppDrawerHeader() {
-  //TODO add your code here
+  Column(
+    modifier = Modifier.fillMaxWidth(),
+    horizontalAlignment = Alignment.CenterHorizontally
+  ) {
+    Image(
+      imageVector = Icons.Filled.AccountCircle,
+      colorFilter = ColorFilter.tint(Color.LightGray),
+      modifier = Modifier
+        .padding(all = 16.dp)
+        .size(50.dp),
+      contentScale = ContentScale.Fit,
+      alignment = Alignment.Center,
+      contentDescription = stringResource(id = R.string.account)
+    )
+    Text(
+      text = stringResource(id = R.string.default_username),
+      color = MaterialTheme.colors.primaryVariant
+    )
+    ProfileInfo()
+  }
+  Divider(
+    color = MaterialTheme.colors.onSurface.copy(alpha = .2f),
+    modifier = Modifier.padding(
+      start = 16.dp,
+      end = 16.dp,
+      top = 16.dp
+    )
+  )
 }
 
 @Composable
-fun ProfileInfo() {
-  //TODO add your code here
+fun ProfileInfo(modifier: Modifier = Modifier) {
+  ConstraintLayout(
+    modifier = modifier
+      .fillMaxWidth()
+      .padding(top = 16.dp)
+  ) {
+    val (karmaItem, divider, ageItem) = createRefs()
+    val colors = MaterialTheme.colors
+
+    ProfileInfoItem(
+      iconAsset = Icons.Filled.Star,
+      amountResourceId = R.string.default_karma_amount,
+      textResourceId = R.string.karma,
+      modifier = modifier.constrainAs(karmaItem) {
+        centerVerticallyTo(other = parent)
+        start.linkTo(anchor = parent.start)
+      }
+    )
+
+    Divider(
+      modifier = modifier
+        .width(width = 1.dp)
+        .constrainAs(ref = divider) {
+          centerVerticallyTo(other = karmaItem)
+          centerHorizontallyTo(other = parent)
+          height = Dimension.fillToConstraints
+        },
+      color = colors.onSurface.copy(alpha = .2f)
+    )
+
+    ProfileInfoItem(
+      iconAsset = Icons.Filled.ShoppingCart,
+      amountResourceId = R.string.default_reddit_age_amount,
+      textResourceId = R.string.reddit_age,
+      modifier = modifier.constrainAs(ref = ageItem) {
+        start.linkTo(anchor = divider.end)
+        centerVerticallyTo(other = parent)
+      }
+    )
+  }
 }
 
 @Composable
@@ -63,7 +140,45 @@ private fun ProfileInfoItem(
   textResourceId: Int,
   modifier: Modifier
 ) {
-  //TODO add your code here
+  val colors = MaterialTheme.colors
+  ConstraintLayout(modifier = modifier) {
+    val (iconRef, amountRef, titleRef) = createRefs()
+    val itemModifier = Modifier
+    Icon(
+      contentDescription = stringResource(id = textResourceId),
+      imageVector = iconAsset,
+      tint = Color.Blue,
+      modifier = itemModifier
+        .constrainAs(ref = iconRef) {
+          centerVerticallyTo(other = parent)
+          start.linkTo(anchor = parent.start)
+        }.padding(start = 16.dp)
+    )
+    Text(
+      text = stringResource(id = amountResourceId),
+      color = colors.primaryVariant,
+      fontSize = 10.sp,
+      modifier = itemModifier
+        .padding(start = 8.dp)
+        .constrainAs(ref = amountRef) {
+          top.linkTo(anchor = iconRef.top)
+          start.linkTo(anchor = iconRef.end)
+          bottom.linkTo(anchor = titleRef.top)
+        }
+    )
+    Text(
+      text = stringResource(textResourceId),
+      color = Color.Gray,
+      fontSize = 10.sp,
+      modifier = itemModifier
+        .padding(start = 8.dp)
+        .constrainAs(ref = titleRef) {
+          top.linkTo(anchor = amountRef.bottom)
+          start.linkTo(anchor = iconRef.end)
+          bottom.linkTo(anchor = iconRef.bottom)
+        }
+    )
+  }
 }
 
 /**
@@ -75,7 +190,22 @@ private fun ProfileInfoItem(
 private fun AppDrawerBody(
   onScreenSelected: (Screen) -> Unit
 ) {
-  //TODO add your code here
+  Column {
+    ScreenNavigationButton(
+      icon = Icons.Filled.AccountBox,
+      label = stringResource(id = R.string.my_profile),
+      onClickAction = {
+        onScreenSelected.invoke(Screen.MyProfile)
+      }
+    )
+    ScreenNavigationButton(
+      icon = Icons.Filled.Home,
+      label = stringResource(R.string.saved),
+      onClickAction = {
+        onScreenSelected.invoke(Screen.Subscriptions)
+      }
+    )
+  }
 }
 
 /**
@@ -130,7 +260,46 @@ private fun ScreenNavigationButton(
  */
 @Composable
 private fun AppDrawerFooter(modifier: Modifier = Modifier) {
-  //TODO add your code here
+  ConstraintLayout(
+    modifier = modifier
+      .fillMaxSize()
+      .padding(start = 16.dp, bottom = 16.dp, end = 16.dp)
+  ) {
+    val colors = MaterialTheme.colors
+    val (settingsImage, settingsText, darkModeButton) = createRefs()
+    Icon(
+      modifier = modifier.constrainAs(ref = settingsImage) {
+        start.linkTo(anchor = parent.start)
+        bottom.linkTo(anchor = parent.bottom)
+      },
+      imageVector = Icons.Default.Settings,
+      contentDescription = stringResource(id = R.string.settings),
+      tint = colors.primaryVariant
+    )
+    Text(
+      fontSize = 10.sp,
+      text = stringResource(R.string.settings),
+      style = MaterialTheme.typography.body2,
+      color = colors.primaryVariant,
+      modifier = modifier
+        .padding(start = 16.dp)
+        .constrainAs(ref = settingsText) {
+          start.linkTo(anchor = settingsImage.end)
+          centerVerticallyTo(other = settingsImage)
+        }
+    )
+    Icon(
+      imageVector = ImageVector.vectorResource(id = R.drawable.ic_moon),
+      contentDescription = stringResource(id = R.string.change_theme),
+      modifier = modifier
+        .clickable { changeTheme() }
+        .constrainAs(ref = darkModeButton) {
+          end.linkTo(anchor = parent.end)
+          bottom.linkTo(anchor = settingsImage.bottom)
+        },
+      tint = colors.primaryVariant
+    )
+  }
 }
 
 private fun changeTheme() {
